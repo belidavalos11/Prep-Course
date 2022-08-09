caf3b30583aae2d4706214fc3815b492fb1bd72c
unknown <belidavalos.bd@gmail.com>
2022-08-09 18:04:19 -0300

    Homework JS3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -69,6 +69,7 @@ function arrayContiene(array, elemento) {
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
   for (var i = 0; i < array.length; i++){
+    
     if (array[i] === elemento) {
       return true
     }
